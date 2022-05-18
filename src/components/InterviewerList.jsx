b9b82eb881d978b07f27b10e4f9c12e185908c40
Nicolas <nchlsreimer@gmail.com>
2022-05-18 16:54:24 -0700

    W7 D3 - Controlled Lists

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -28,8 +28,8 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
-        selected={interviewer.id === props.interviewer}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
