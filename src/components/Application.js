dbebe6635545d82fd8d6a2ff6a7bcfc2e13505b9
Nicolas <nchlsreimer@gmail.com>
2022-05-18 09:31:05 -0700

    W7 D2 - Updating our App

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 import "components/Application.scss";
+import DayList from "components/DayList";
+
+// MOCK DATA: it gets fed into the Application component below
+// -the result of Application using mock data renders in the nav html tag
+// -in the future this would be replaced by an api
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
@@ -12,7 +34,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={(day) => console.log(day)}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
